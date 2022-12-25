8a85feb474ab0753c34b3231be7d32226fd2cd96
AhmedZaghdoudi <63302891+AhmedZaghdoudi@users.noreply.github.com>
2022-12-25 11:47:30 +0100

    Update constatnts.dart

--- lib/shared/constatnts.dart
+++ lib/shared/constatnts.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/animation.dart';
 
 class constants {
-  static String appId = "1:646097709731:web:79ff66f501df585dfdad16";
-  static String apiKey = "AIzaSyC3ryUlxuFXvhSrACDMu4ebFxn8irvQC9I";
-  static String messagingSenderId = "646097709731";
-  static String ProjectId = "chatappflutter-1ced7";
+  static String appId = "";
+  static String apiKey = "";
+  static String messagingSenderId = "";
+  static String ProjectId = "";
   final primaryColor = const Color(0xFFee7b64);
 }
